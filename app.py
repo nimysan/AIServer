93a1b82e15b175ad67b99948a36803c6934f61d0
nimysan <234aini@gmail.com>
2024-05-16 11:16:39 +0800

    setup rolling file handler

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import logging
-import logging
 import os
 
 from flask import Flask, send_from_directory
@@ -9,6 +8,21 @@ from flask_cors import CORS
 import config
 from endpoints import register_api_endpoints
 from model import init_model_access, get_user_repository
+
+from logging.handlers import RotatingFileHandler
+
+# 创建一个日志文件处理程序
+file_handler = RotatingFileHandler('flask_app.log', maxBytes=10000, backupCount=5)
+file_handler.setLevel(logging.INFO)
+
+# 创建一个控制台日志处理程序
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# 设置日志格式
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -29,8 +43,6 @@ def server_react_as_webui(app):
             return send_from_directory(app.static_folder, 'index.html')
 
 
-
-
 def create_app():
     # 同时部署/api和ui dashboard
     app = Flask(__name__, static_folder='../ai-frontend/dist')
@@ -49,12 +61,7 @@ def create_app():
 
     server_react_as_webui(app)
 
-
-
     return app
-
-
-
 
 
 if __name__ == '__main__':
