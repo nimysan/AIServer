c68f017fe4e68dc287b4081a501b70362695fe18
nimysan <234aini@gmail.com>
2024-11-27 15:45:58 +0800

    parse products and solor specifications

--- offline_tools/rules/read_rules.py
+++ offline_tools/rules/read_rules.py
@@ -1,27 +1,27 @@
 import json
-
 import pandas as pd
 
 # 读取Excel表格数据
-df = pd.read_excel('rules.xlsx', sheet_name='整理后的订单处理规则', header=0)
-
-# 将预售订单标记为1,非预售订单标记为0
-# df['is_pre_sales_order'] = df['is_pre_sales_order'].map({'yes': 1, 'no': 0})
+df = pd.read_excel('修改订单处理规则1126.xlsx', sheet_name='意图规则', header=0)
 
 # 将NaN替换为空字符串
 df = df.fillna('')
+
+# 拆分order_status为多个值
+df = df.assign(order_status=df['order_status'].str.split(',')).explode('order_status')
 
 # 生成规则对象
 rules = {}
 for _, row in df.iterrows():
     intent = row['intent']
-    order_status = row['order_status']
+    order_status = row['order_status'].strip()  # 去除空格
     is_pre_sales_order = row['is_pre_sales_order']
     order_channel = row['order_channel']
     rule_for_handle = row['rule_for_handle']
     customer_reply_rule = row['customer_reply_rule']
     agent_action = row['agent_action']
     target_ticket_status = row['target_ticket_status']
+    target_ticket_label = row['ticket_label']
 
     if intent not in rules:
         rules[intent] = {}
@@ -42,11 +42,3 @@ for _, row in df.iterrows():
 # Write the JSON data to a file
 with open('rules.json', 'w', encoding='utf-8') as file:
     file.write(json.dumps(rules, indent=4, ensure_ascii=False))
-
-
-def get_rule(intent, order_status, order_channel, is_pre_sales_order):
-    try:
-        return rules[intent][order_status][is_pre_sales_order][order_channel]
-    except KeyError:
-        return {'rule_for_handle': 'No matching rule found', 'customer_reply_rule': None, 'agent_action': None,
-                'target_ticket_status': None}
--- offline_tools/rules/rule.py
+++ offline_tools/rules/rule.py
@@ -1,69 +1,193 @@
 rules = {
-    'modify_order_item_details': {
-        'not_converted': {
-            'shopify': {'rule': '该订单未支付或者预售订单,请等待客服处理', 'next_ticket_status': 'open'},
-            'others': {'rule': '该订单未支付或者预售订单,请等待客服处理', 'next_ticket_status': 'open'}
+    "modify_delivery_address": {
+        "Shipped": {
+            "true": {
+                "Shopify": {
+                    "rule_for_handle": "该订单已发货，无法处理",
+                    "customer_reply_rule": "该订单已发货，请联系物流公司确认，提供该订单物流单号",
+                    "agent_action": "无需处理",
+                    "target_ticket_status": "solved"
+                },
+                "Others": {
+                    "rule_for_handle": "该订单已发货，无法处理",
+                    "customer_reply_rule": "该订单已发货，请联系物流公司确认，提供该订单物流单号",
+                    "agent_action": "无需处理",
+                    "target_ticket_status": "solved"
+                }
+            },
+            "false": {
+                "Shopify": {
+                    "rule_for_handle": "该订单已发货，无法处理",
+                    "customer_reply_rule": "该订单已发货，请联系物流公司确认，提供该订单物流单号",
+                    "agent_action": "无需处理",
+                    "target_ticket_status": "solved"
+                },
+                "Others": {
+                    "rule_for_handle": "该订单已发货，无法处理",
+                    "customer_reply_rule": "该订单已发货，请联系物流公司确认，提供该订单物流单号",
+                    "agent_action": "无需处理",
+                    "target_ticket_status": "solved"
+                }
+            }
         },
-        'canceled': {
-            'shopify': {'rule': '订单已取消,不支持处理', 'next_ticket_status': 'solved'},
-            'others': {'rule': '订单已取消,不支持处理', 'next_ticket_status': 'solved'}
-        }
-    },
-    'increase_order_item_quantity': {
-        'pending': {
-            'shopify': {'rule': '可尝试联系shopify单后重新下单', 'next_ticket_status': 'solved'},
-            'others': {'rule': '不支持处理', 'next_ticket_status': 'solved'}
-        }
-    },
-    'modify_payment_method': {
-        'pending': {
-            'shopify': {'rule': 'Shopify订单可尝试联系订单员#Shopify订单不支持任何操作',
-                        'next_ticket_status': 'solved'},
-            'others': {'rule': '可尝试控制订单后人工修改', 'next_ticket_status': 'solved'}
+        "Pending": {
+            "true": {
+                "Shopify": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                },
+                "Others": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                }
+            },
+            "false": {
+                "Shopify": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                },
+                "Others": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                }
+            }
         },
-        'awaiting_shipment': {
-            'shopify': {'rule': '建议取消后重新下单', 'next_ticket_status': 'solved'},
-            'others': {'rule': '不可做任何操作', 'next_ticket_status': 'solved'}
-        }
-    },
-    'modify_delivery_address': {
-        'pending': {
-            'shopify': {'rule': 'Shopify订单可尝试联系订单员#Shopify订单不支持任何操作',
-                        'next_ticket_status': 'solved'},
-            'others': {'rule': '可尝试控制订单后人工修改', 'next_ticket_status': 'solved'}
+        "Awaiting Allocation": {
+            "true": {
+                "Shopify": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                },
+                "Others": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                }
+            },
+            "false": {
+                "Shopify": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                },
+                "Others": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                }
+            }
         },
-        'awaiting_shipment': {
-            'shopify': {'rule': '获取用户新地址， 拦截订单，根据拦截订单状态回复客户', 'next_ticket_status': 'solved'},
-            'others': {'rule': '获取用户新地址， 拦截订单，根据拦截订单状态回复客户', 'next_ticket_status': 'solved'}
-        }
-    },
-    'modify_delivery_time': {
-        'pending': {
-            'shopify': {'rule': 'Shopify订单可尝试联系订单员#Shopify订单不支持任何操作',
-                        'next_ticket_status': 'solved'},
-            'others': {'rule': '可尝试控制订单后人工修改', 'next_ticket_status': 'solved'}
-        }
-    },
-    'decrease_order_item_quantity': {
-        'canceled': {
-            'shopify': {'rule': '订单已终极,提供物流信息,建议用户自行联系物流公司', 'next_ticket_status': 'solved'},
-            'others': {'rule': '订单已终极,提供物流信息,建议用户自行联系物流公司', 'next_ticket_status': 'solved'}
+        "Ready to Ship": {
+            "true": {
+                "Shopify": {
+                    "rule_for_handle": """
+                        1. 确认是否有明确待修改的地址 2. 拦截订单 
+                    """,
+                    "customer_reply_rule": "确认地址->拦截订单->交由人工坐席处理 1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                },
+                "Others": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                }
+            },
+            "false": {
+                "Shopify": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                },
+                "Others": {
+                    "rule_for_handle": "1.如果用户提及了要修改的地址、姓名或者电话号码,尝试拦截订单\n2.如果未提及要修改的信息，仅需跟用户确认，无需拦截订单",
+                    "customer_reply_rule": "1.我们尝试拦截订单，但是拦截失败(Failed Interception)，我们无法修改配送地址，请联系物流公司确认，提供该订单物流单号\n2.我们正在尝试拦截订单（Not Intercepted、Intercepting、Partial Success、Successful Interception），请等待客服处理",
+                    "agent_action": "1.订单拦截失败、无需处理\n2.请确认订单拦截结果，并跟进处理",
+                    "target_ticket_status": "1.solved\n2.open"
+                }
+            }
         }
     }
 }
+#通用规则
+def get_rule_by_order_status(order_status):
+    rules = {
+        'Untransferred': {
+            'rule_for_handle': '不处理',
+            'customer_reply_rule': '订单尚未处理,请耐心等待',
+            'agent_action': '订单尚未处理,不需要采取任何行动',
+            'target_ticket_status': 'Open',
+            'ticket_label': 'ai-auto-require-hum'
+        },
+        'Cancelled': {
+            'rule_for_handle': '无法处理',
+            'customer_reply_rule': '订单已取消,无法处理',
+            'agent_action': '订单已取消,无需进一步操作',
+            'target_ticket_status': 'solved',
+            'ticket_label': 'ai-auto-solved'
+        },
+        'Not_mentioned': {
+            'rule_for_handle': '无法处理',
+            'customer_reply_rule': '请提供订单号,以便我们协助处理',
+            'agent_action': '无法确认订单状态,请联系客户获取更多信息',
+            'target_ticket_status': 'solved',
+            'ticket_label': 'ai-auto-solved'
+        },
+        'Not_found': {
+            'rule_for_handle': '无法处理',
+            'customer_reply_rule': '请提供订单号,以便我们协助处理',
+            'agent_action': '无法确认订单状态,请联系客户获取更多信息',
+            'target_ticket_status': 'solved',
+            'ticket_label': 'ai-auto-solved'
+        }
+    }
 
+    if order_status in rules:
+        return rules[order_status]
 
-def get_rule(intent, order_status, order_channel):
-    if intent in rules and order_status in rules[intent] and order_channel in rules[intent][order_status]:
-        return rules[intent][order_status][order_channel]
-    else:
+def get_rule(intent, order_status, order_channel, is_pre_sales_order):
+    try:
+        return rules[intent][order_status][is_pre_sales_order][order_channel]
+    except KeyError:
         return {
-            "rule": "no matched rule",
-            "next_ticket_status": "open"
+            'rule_for_handle': 'No matching rule found',
+            'customer_reply_rule': None,
+            'agent_action': None,
+            'target_ticket_status': None
         }
 
 
-def main(intent: str, status: str, order_channel: str) -> dict:
+def main(intent: str,
+         order_status: str,
+         is_pre_sales_order: str,
+         order_channel: str) -> dict:
+    rule = get_rule_by_order_status(order_status)
+    if rule is None:
+        rule = get_rule(intent, order_status, order_channel, is_pre_sales_order)
+        no_rule = str(rule['rule_for_handle'] == 'No matching rule found')
+    else:
+        no_rule = str(rule['rule_for_handle'] == 'No matching rule found')
+
     return {
-        'rule': get_rule(intent, status, order_channel)
+        'rule': rule,  # Object
+        'no_rule': no_rule  # String
     }
+
+
+rule = main("modify_delivery_address","awaiting_shipment","true","Shopify")
+print(rule)
